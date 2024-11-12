c3187146ab73bdaeb80a3bdeecb2bce915f1a5c5
Christian John jiménez Calvo <christianjjc@gmail.com>
2024-11-12 13:17:08 -0500

    primer reporte lanzado HOLA MUNDO OK!

--- src/basic-reports/basic-reports.service.ts
+++ src/basic-reports/basic-reports.service.ts
@@ -1,6 +1,18 @@
 import { BadRequestException, Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
+import PdfPrinter from 'pdfmake';
+import type { TDocumentDefinitions } from 'pdfmake/interfaces';
+
+const fonts = {
+  Roboto: {
+    normal: 'fonts/Roboto-Regular.ttf',
+    bold: 'fonts/Roboto-Bold.ttf',
+    italics: 'fonts/Roboto-Italic.ttf',
+    bolditalics: 'fonts/Roboto-BoldItalic.ttf',
+  },
+};
+
 @Injectable()
 export class BasicReportsService extends PrismaClient implements OnModuleInit {
   private readonly logger = new Logger('OrdersService');
@@ -16,5 +28,17 @@ export class BasicReportsService extends PrismaClient implements OnModuleInit {
       throw new BadRequestException('No registers on DataBase');
     }
     return employees;
+  }
+
+  hello() {
+    const printer = new PdfPrinter(fonts);
+
+    const docDefinition: TDocumentDefinitions = {
+      content: ['Hola Mundo'],
+    };
+
+    const doc = printer.createPdfKitDocument(docDefinition);
+
+    return doc;
   }
 }
