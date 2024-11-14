95d665abaabc270edb91a8bcdeb416bc6f511351
Christian John jiménez Calvo <christianjjc@gmail.com>
2024-11-13 22:39:43 -0500

    seccion-05-ep-08-codigo-qr OK!

--- src/reports/order-ticket-by-id.report.ts
+++ src/reports/order-ticket-by-id.report.ts
@@ -21,16 +21,17 @@ export const getOrderTicketById = () => {
     header: logo,
     pageMargins: [40, 60, 40, 60],
     content: [
-      //
+      //* Header
       {
         text: 'Tucan Code',
         style: 'header',
       },
+      //* Address
       {
         columns: [
           //
           {
-            text: `15 Montgomery Str, Suite 100, \nOttawa ON K2Y 9X1, CANADA \nBN: 12783671823 \nhttps://devtalles.com`,
+            text: `15 Montgomery Str, Suite 100, \nOttawa ON K2Y 9X1, CANADA \nBN: 12783671823 \nhttps://cjjc.pe`,
             alignment: 'left',
             width: '50%',
           },
@@ -41,7 +42,30 @@ export const getOrderTicketById = () => {
           },
         ],
       },
+      //* QR Code
+      // basic usage
+      // { qr: 'https://cjjc.pe' },
+      //* colored QR
+      // { qr: 'text in QR', foreground: 'red', background: 'yellow' },
+      //* resized QR
+      { qr: 'https://cjjc.pe', fit: 100, alignment: 'right' },
     ],
   };
   return docDefinition;
 };
+
+/*
+Properties:
+qr: string - text in QR code
+foreground: string (optional, default black) - foreground color
+background: string (optional, default white) - background color
+fit: integer (optional) - fit the output QR image
+version: integer (optional) - QR version range from 1 to 40 (for details see wikipedia.org
+eccLevel: string (optional, default L) - error correction capability (for details see wikipedia.org), possible values:
+L - Level L (Low), approx 7% of codewords can be restored
+M - Level M (Medium), approx 15% of codewords can be restored
+Q - Level Q (Quartile), approx 25% of codewords can be restored
+H - Level H (High), approx 30% of codewords can be restored
+mode: string (optional) - encoding mode, possible values: numeric, alphanumeric, octet (for details see wikipedia.org)
+mask: integer (optional) - mask pattern, range from 0 to 7 (for details see wikipedia.org)
+ */
